07c6d01f80c2908c40ba48870529d374d9df7a84
Karen Polanco <polanco.k@gmail.com>
2022-12-31 19:11:11 -0500

    commit message

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 My Book Tracker
+https://gist.github.com/cblunt/860360 <-- cheat sheet remove before submitting 
+
+https://discuss.codecademy.com/t/what-is-the-difference-between-thead-and-th/308448 <--thead reference, for colspan
--- index.js
+++ index.js
@@ -1,31 +1,69 @@
 //document.addEventListener("DOMContentLoaded", init)
 
+
+
 function submitListner(bookData){
 document.querySelector("form").addEventListener("submit", e => {
     e.preventDefault();
    let textBoxValue = e.target.textBox.value
-   //console.log(textBoxValue)
-    //return textBoxValue //how to connect this to the search
-    fetch(`https://openlibrary.org/search.json?q=${textBoxValue}`, {
+  //console.log(textBoxValue)
+    fetch(`https://openlibrary.org/search.json?q=${textBoxValue}&fields=title,author_name&limit=10`, {
         method: "GET"
         })
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        //the below works
-        let targeting = data.docs[0]
-        let found = targeting.title
-        let para = document.createElement("p")
-        para.innerText = found
-        document.getElementById("searchResults").appendChild(para)
-    }
-    )
+        let dataArr = Object.values(data);
+        console.log(dataArr)
+        let resultValues = dataArr[3][0]//b/c i have 0, i only get the first option
+       console.log(resultValues)
+       for(let e in resultValues){
+        //console.log(e)
+        if(e === "title" || "author_name"){
+           let result = resultValues[e]
+           console.log(result)
+        } 
+        }
+       }
+    //    let test = Object.values(resultValues)
+    //    console.log(test)
+        
+
+
+      
+    //    //searchResults(resultValues)
+    );
     }
 )}
 
-    submitListner()
+    submitListner();
+
+// function searchResults(book){
+//     let para = document.createElement("p")
+//     for(let obj of book){
+//         console.log(obj) //undefined
+//         console.log(book)
+//         let titleMatch = obj.title
+//         let authorMatch = obj.author_name
+//         //console.log(titleMatch || authorMatch)
+//         }
 
 
+    // book.forEach(e => {
+    //  console.log(e.title)
+     
+    // });
+//}
+
+
+
+
+           //the below works
+        // let targeting = data.docs[0]
+        // let found = targeting.title
+        // 
+        // para.innerText = found
+        // document.getElementById("searchResults").appendChild(para)
 
 
 //maybe something like if(title/author === textBoxValue return that info) put this in the getAPI function
