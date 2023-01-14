9ce0bcfe5e0b0476d1d3dc84794ef15d2ee30bc0
Karen Polanco <polanco.k@gmail.com>
2023-01-13 20:17:12 -0500

    commit message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () =>{
   snagForm.addEventListener("submit", (e) => {
     e.preventDefault()
     let textBoxValue = e.target.textBox.value
-    fetch(`http://openlibrary.org/search.json?author=${textBoxValue}&limit=2`, {
+    fetch(`http://openlibrary.org/search.json?author=${textBoxValue}&limit=5`, {
       method: "GET"
     })
     .then(res => res.json())
@@ -12,26 +12,17 @@ document.addEventListener("DOMContentLoaded", () =>{
       console.log(data.docs) 
       let booksArr = data.docs
       console.log(booksArr)
+
       let displayHere = document.getElementById("searchResultsGoHere")
       let table = document.createElement("table")
-      let headerRow = document.createElement("tr")
-      let createCell = document.createElement("th")
-     
-      let headers = ["Title: ", " Author:"]
-
-      headers.forEach(headerText => {
-        let textCell = document.createTextNode(headerText)
-        createCell.appendChild(textCell)
-        headerRow.appendChild(createCell)
-      })
-
-      table.appendChild(headerRow)
+   
       displayHere.appendChild(table)
-
-      for(let e in booksArr){
       
-        let authorName = booksArr[e].author_name 
-        let bookTitle = booksArr[e].title
+      booksArr.forEach(book => {
+        console.log(book.author_name)
+
+        let authorName = book.author_name
+        let bookTitle = book.title
        
         let createRow = document.createElement("tr")
         let titleCell = document.createElement("td")
@@ -41,8 +32,9 @@ document.addEventListener("DOMContentLoaded", () =>{
         createRow.appendChild(titleCell)
         createRow.appendChild(authorCell)
         table.appendChild(createRow)
-        snagForm.reset() 
-      }
+        snagForm.reset()
+      })
+
 
       let snagClear = snagForm.querySelector("#revert")
       snagClear.addEventListener("click", e => {
